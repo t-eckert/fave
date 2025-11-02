63a0db2f1950504a7f73757600bb222a3259c623
Thomas Eckert <thomas.james.eckert@gmail.com>
2025-11-01 23:11:15 -0400

    Subcommand handling

--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"github.com/t-eckert/fave/cmd"
+
+	"fmt"
+	"os"
 )
 
 func main() {
-	cmd.RunServer()
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: fave <subcommand>")
+		os.Exit(1)
+	}
+
+	args := os.Args[1:]
+	subcommand := args[0]
+	switch subcommand {
+	case "serve":
+		cmd.RunServer()
+	}
 }
